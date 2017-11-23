18cbe92839af7088da4ce11448f705e25915d478
carlaesteban <carla.esteban@telefonica.net>
2017-11-23 22:02:38 +0100

    he cambiado 55555

--- js/scater.js
+++ js/scater.js
@@ -2,7 +2,7 @@ console.log("hola consola");
 
 d3.json("datos.json", function(data){
         
-        console.log("hola consola2")
+        console.log("hola consola5555552")
     
         window.data = data
         
